1806e7f85abc0d6081a7de0d3bd0f9bd1aeb37ca
Evrim Uysal <evrim.uysal@yahoo.com>
2025-01-21 22:03:46 +0300

    - Mongodb conn string changed - Heroku CI/CD added

--- main.go
+++ main.go
@@ -3,18 +3,40 @@ package main
 import (
 	"gin-mongo-api/configs"
 	"gin-mongo-api/routes"
+	"log"
+	"os"
 
 	"github.com/gin-gonic/gin"
 )
 
+// Error message as a constant
+const ErrPortNotSet = "$PORT must be set"
+
 func main() {
+	portEnv := os.Getenv("PORT")
+	if portEnv == "" {
+		log.Fatal(ErrPortNotSet)
+	}
+
+	// Initialize router
+	router := setupRouter()
+
+	// Start the server
+	router.Run(":" + portEnv)
+}
+
+// setupRouter sets up the Gin router and initializes configurations and routes
+func setupRouter() *gin.Engine {
 	router := gin.Default()
 
-	//run database
+	// Connect to the database
 	configs.ConnectDB()
 
-	//routes
+	// Apply middleware
+	router.Use(gin.Logger())
+
+	// Register routes
 	routes.UserRoute(router)
 
-	router.Run("localhost:8080")
+	return router
 }
